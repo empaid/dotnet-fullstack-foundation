46810a88d652fab509dfff2d4f14c4e7fd12aa2a
empaid <empaid@empaids-MBP.modem>
2023-11-16 14:12:58 +1100

    array iteration

--- Program.cs
+++ Program.cs
@@ -13,3 +13,17 @@ int[][] myArray = new int[3][];
 myArray[0] = new int[2] {1, 2};
 myArray[1] = new int[3] {3, 4, 5};
 myArray[2] = new int[1] {6};
+
+//array manipulation
+int[] marks = new int[5] {1,2,3,4,5};
+foreach(int mark in marks){
+    Console.WriteLine(mark);
+}
+
+int[,] multi = new int[3,4] {{1,2,3,4}, {1,2,3,4}, {1,2,3,4}};
+for(int i=0; i<3; i++){
+    for(int j=0; j<4; j++){
+        Console.Write(multi[i, j] + " ");
+    }
+    Console.Write("\n");
+}
